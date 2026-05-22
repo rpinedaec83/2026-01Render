1e44a192c2dd549e7df238df04a274066c14648e
Roberto Pineda <rpineda@x-codec.net>
2026-05-21 22:21:29 -0500

    Primer Despliegue

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ require('./src/routes/auth.routes')(app);
 require('./src/routes/user.routes')(app);
 
 app.get('/',(req,res)=>{
-    res.send('Hola desde render');
+    res.send('Hola desde desde Azure Primera Vez');
 })
 
 const db = require('./src/models');
